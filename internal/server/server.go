a55e0b5543b0eaa8617ed405827818111c3e014e
AlexFox86 <alek-fokin@yandex.ru>
2025-08-25 19:29:48 +0300

    add packages

---
+++ internal/server/server.go
@@ -0,0 +1,20 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+// Start starts the server
+func Start() error {
+	webDir := "./web"
+	http.Handle("/", http.FileServer(http.Dir(webDir)))
+
+	port := os.Getenv("TODO_PORT")
+	port = ":7540"
+
+	fmt.Println("Server is running on port", port)
+
+	return http.ListenAndServe(port, nil)
+}
--- main.go
+++ main.go
@@ -1,22 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
-	"os"
+
+	"github.com/AlexFox86/scheduler/internal/server"
 )
 
 func main() {
-	//http.HandleFunc("GET /validate", handler.Validate)
-	webDir := "./web"
-	http.Handle("/", http.FileServer(http.Dir(webDir)))
-	http.Handle("/js/scripts.min.js", http.FileServer(http.Dir(webDir)))
-	http.Handle("/css/style.css", http.FileServer(http.Dir(webDir)))
-	http.Handle("/favicon.ico", http.FileServer(http.Dir(webDir)))
-
-	port := os.Getenv("AUTH_PORT")
-	port = ":7540"
-	fmt.Println("Server is running on port", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	log.Fatal(server.Start())
 }
